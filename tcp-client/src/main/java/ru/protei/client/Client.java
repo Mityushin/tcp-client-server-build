c7a73c868d058e5e514a71f847a473ee0ca0c9c0
Дмитрий Митюшин <mityushin.dmitry@gmail.com>
2018-06-27 14:36:38 +0300

    remove System.out

--- tcp-client/src/main/java/ru/protei/client/Client.java
+++ tcp-client/src/main/java/ru/protei/client/Client.java
@@ -27,7 +27,6 @@ public class Client {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        Logger.getRootLogger().setLevel(Level.FATAL);
         new Client().run();
     }
 
